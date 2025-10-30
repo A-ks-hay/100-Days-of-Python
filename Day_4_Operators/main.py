04e7e2de5ab60483a46c1bed4bad87f567469c64
A-ks-hay <kumaraashu29508@gmail.com>
2025-10-30 19:22:21 +0530

    Update main.py

--- Day_4_Operators/main.py
+++ Day_4_Operators/main.py
@@ -21,4 +21,82 @@ print("Floor Division:", a // b)
 print("Modulus:", a % b) 
 
 # Exponentiation
+
 print("Exponentiation:", a ** b)
+
+#Comparison Operator
+a = 13
+b = 33
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+#Logical Operator
+
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
+
+#Bitwise Operator
+
+a = 10
+b = 4
+
+print(a & b)
+print(a | b)
+print(~a)
+print(a ^ b)
+print(a >> 2)
+print(a << 2)
+
+#Assignment Operator
+
+a = 10
+b = a
+print(b)
+b += a
+print(b)
+b -= a
+print(b)
+b *= a
+print(b)
+b <<= a
+print(b)
+
+#Identity Operator
+
+a = 10
+b = 20
+c = a
+
+print(a is not b)
+print(a is c)
+
+#Membership Operator
+
+x = 24
+y = 20
+list = [10, 20, 30, 40, 50]
+
+if (x not in list):
+    print("x is NOT present in given list")
+else:
+    print("x is present in given list")
+
+if (y in list):
+    print("y is present in given list")
+else:
+    print("y is NOT present in given list")
+
+#Ternary Operator
+
+a, b = 10, 20
+min = a if a < b else b
+
+print(min)
